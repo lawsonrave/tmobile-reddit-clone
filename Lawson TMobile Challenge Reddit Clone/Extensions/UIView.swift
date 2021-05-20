b3e9f78897874f2614cba3fc98a6b4a2d7820427
ravebizz <ravebizz@ravebizzs-Air.local.tld>
2021-05-20 08:57:56 -0400

    Refactor addSubviews.

---
+++ Lawson TMobile Challenge Reddit Clone/Extensions/UIView.swift
@@ -0,0 +1,18 @@
+//
+//  UIView.swift
+//  Lawson TMobile Challenge Reddit Clone
+//
+//  Created by ravebizz on 5/20/21.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func addSubViews(_ views: UIView ...) {
+        views.forEach {
+            
+        }
+    }
+    
+}
